c34948d303eaf03cff45338bf6d6751cf8824724
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-05-14 23:18:49 -0400

    added employees component and tested connection to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import API from './utils/API.js'
+import Employees from "./components/Employees"
 
 function App() {
   console.log(API.getRandomEmployee())
   return (
-  <div>
-    <h1>Employee Directory</h1>
-  </div>
+  
+      // <h1>Employee Directory</h1>
+      <Employees />
+   
   );
 }
 
