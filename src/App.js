6ddb75087b1cd903b744f71947d4efe60a442137
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-05-19 19:02:10 -0400

    removed searchbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Container from "./components/Container";
 import Header from "./components/Header";
-import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div>
       <Header />
-      <SearchBar />
       <Container />
     </div>
   );
